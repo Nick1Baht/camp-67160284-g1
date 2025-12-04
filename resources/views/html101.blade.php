247547cf930cfacd2ec3a744d52b11824288596e
Nick1Baht <lucifernicky5@gmail.com>
2025-12-04 19:14:05 +0700

    Pre-Workshop 1

---
+++ resources/views/html101.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href ="css/bootstrap.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Itim&display=swap" rel="stylesheet">
+    <style>
+        body {font-family: "Itim", cursive;}
+    </style>
+</head>
+    <body>
+        <div class="container mt-4">
+            <h1>Workshop #HTML - FORM</h1>
+                <form>
+                    <div class="row">
+                        <div class="col-sm-12 col-md-4">
+                        <label for="fname">ชื่อ</label>
+                    </div>
+                    <div class="col-8">
+                        <input id = "fname" class="form-control">
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-12 col-md-4">
+                        <label for="lname">สกุล</label>
+                    </div>
+                    <div class="col-8">
+                        <input id = "lname" class="form-control">
+                    </div>
+
+                </form>
+        </div>
+    </body>
+</html>
